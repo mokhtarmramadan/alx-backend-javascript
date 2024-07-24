025d0de3a78ebfcfc7476687f108f03b8631f7c3
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-07-24 20:14:15 +0300

    Added 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * print in stdout the message
+ * @param: {string} - message to be printed
+ * @returns: {void}
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+/**
+ * exports teh function
+ */
+module.exports = displayMessage;
