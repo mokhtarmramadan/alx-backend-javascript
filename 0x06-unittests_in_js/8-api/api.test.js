a0a6380ecf35d57631573c4d4b966f03e166c4b2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-07-25 21:45:00 +0300

    07-25 21:45

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,14 @@
-const request = require('supertest');
+const request = require('request');
 const { expect } = require('chai');
-const app = require('../app'); // Adjust the path as necessary
 
-describe('API integration test', function() {
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
   it('GET / returns correct response', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .expect('Content-Type', /text/)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
-
